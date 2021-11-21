beb1fe67c4e8796955da39a3c08019b0e21b82eb
NoelAndrew <andrew98noel@gmail.com>
2021-11-21 17:48:53 -0600

    react-bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import '@/styles/global.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
+import 'bootstrap/dist/css/bootstrap.css'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
